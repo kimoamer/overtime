aca174003c06c1fc865122588122de592f66983b
kimoamer <farisamer2020@gmail.com>
2025-04-03 17:11:02 +0200

    update

--- overtime/overrides/salary_slip.py
+++ overtime/overrides/salary_slip.py
@@ -57,7 +57,6 @@ class SalarySlipNew(SalarySlip):
                 )
                 self.set_time_sheet()
                 self.pull_sal_struct()
-            frappe.msgprint(f"{self.posting_date}")
             process_loan_interest_accruals(self)
                
     def get_working_days_details(self, lwp=None, for_preview=0):
